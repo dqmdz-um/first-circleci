a05e5eacf59b2744b4fd03ab759cba547dd6b849
dqmdz-um <daniel.quinteros@um.edu.ar>
2022-09-14 12:01:19 -0300

    Badge CircleCI

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # first-circleci
+
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/dqmdz-um/first-circleci/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/dqmdz-um/first-circleci/tree/main)
--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from factorial import factorial
 
 class TestFactorial(unittest.TestCase):
     def test_factorial(self):
-        self.assertEqual(25, factorial(4))
+        self.assertEqual(24, factorial(4))
 
 
 if __name__ == '__main__':
